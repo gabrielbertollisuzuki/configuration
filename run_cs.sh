0f3f3c04d5bdf2cbd6eefef3bc897c1aba43ce45
GBSuzuki <gabriel.suzuki7@gmail.com>
2016-07-14 18:59:45 -0300

    Add files via upload

---
+++ run_cs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --primary --mode 1024x576 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
+steam steam://run/730
